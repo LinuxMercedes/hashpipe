eb9f5870bd718299cf87b335a8347d31c440247f
LinuxMercedes <LinuxMercedes@gmail.com>
2016-12-28 21:17:37 -0600

    Add raw input parsing

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ extern crate irc;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-use std::default::Default;
 use irc::client::prelude::*;
+use std::default::Default;
+use std::str::FromStr;
 
 use chan_signal::Signal;
 use std::thread::spawn;
@@ -27,7 +28,10 @@ fn main() {
                             (@arg server: -s --server +required +takes_value "IRC server to connect to")
                             (@arg nick: -n --nick +takes_value "Nickname to use")
                             (@arg channels: -c --channels +takes_value "Channel(s) to speak in (if not in raw mode)")
+                            // NOTE: long() is a required workaround for parsing long options with
+                            // hyphens; see https://github.com/kbknapp/clap-rs/issues/321
                             (@arg raw_out: -o long("--raw-out") "Echo everything from the IRC server directly")
+                            (@arg raw_in: -i long("--raw-in") "Interpret STDIN as raw IRC commands")
                            ).get_matches();
 
     let nick = matches.value_of("nick").unwrap_or("hashpipe").to_string();
@@ -35,6 +39,7 @@ fn main() {
     let channels :Vec<String> = matches.value_of("channels").unwrap_or("#hashpipe").split(",").map(|x| x.to_string()).collect();
 
     let raw_out = matches.is_present("raw_out");
+    let raw_in = matches.is_present("raw_in");
 
     let cfg = Config {
         nickname: Some(nick),
@@ -71,13 +76,16 @@ fn main() {
     let io_server = server.clone();
     let (sio, rio) = chan::sync(0);
 
-    spawn (move || run_io(io_server, channels, sio));
+    spawn (move || run_io(io_server, channels, raw_in, sio));
 
     chan_select! {
         signal.recv() -> _signal => {
             /* Falls through to quit after this select block */
         },
         rio.recv() => {
+            /* Falls through to quit after this select block */
+        },
+        rirc.recv() => {
             /* Falls through to quit after this select block */
         },
     }
@@ -111,12 +119,19 @@ fn run_irc(server: IrcServer, raw:bool, sjoin: chan::Sender<()>) {
 /*
  * Read stdin and write each line to all channels
  */
-fn run_io(server: IrcServer, channels: Vec<String>, _sdone: chan::Sender<()>) {
+fn run_io(server: IrcServer, channels: Vec<String>, raw:bool, _sdone: chan::Sender<()>) {
     let stdin = BufReader::new(std::io::stdin());
     for line in stdin.lines() {
         let ln = line.unwrap();
-        for channel in &channels {
-            server.send_privmsg(&channel, &ln).unwrap()
+        if raw {
+            let raw_line = ln + "\r\n"; // IRC line terminator
+            let msg = Message::from_str(&raw_line).unwrap();
+            server.send(msg).unwrap();
+        }
+        else {
+            for channel in &channels {
+                server.send_privmsg(&channel, &ln).unwrap()
+            }
         }
     }
     // When this function ends, it drops _sdone, signaling main
